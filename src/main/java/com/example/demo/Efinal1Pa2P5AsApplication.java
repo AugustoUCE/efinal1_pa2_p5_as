2c0f03ffe2f76d78fbf35642940a3161a74ec4fb
AugustoUCE <aysalaza@uce.edu.ec>
2024-01-15 18:27:36 -0500

    prueba2

---
+++ src/main/java/com/example/demo/Efinal1Pa2P5AsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Efinal1Pa2P5AsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Efinal1Pa2P5AsApplication.class, args);
+	}
+
+}
